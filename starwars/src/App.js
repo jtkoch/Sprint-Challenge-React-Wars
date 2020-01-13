7aa25055bb7920dedcaa44235aa5ce09cafb65f3
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2020-01-12 18:27:39 -0600

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import StarWarsList from "./components/StarWarsList";
 import styled from "styled-components";
 
+
 const Header = styled.h1`
 color: maroon;
 font-family: 'Bungee Inline', cursive;
@@ -10,13 +11,6 @@ font-size: 80px;
 `
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
   return (
     <div className="App">
       <Header>React Wars</Header>
